5988bcd670709f553d49a803ba210b9a52a40b31
Jeremy Townley <jetownle@e1z1r7p32.42.us.org>
2019-08-14 07:19:11 -0700

    sweatcha out

--- mlx.c
+++ mlx.c
@@ -6,7 +6,7 @@
 /*   By: jetownle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 03:46:58 by jetownle          #+#    #+#             */
-/*   Updated: 2019/08/14 05:39:47 by jetownle         ###   ########.fr       */
+/*   Updated: 2019/08/14 07:18:52 by jetownle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int		render(t_fdf *fdf)
 {
 
 	fdf->mlx.img = mlx_new_image(fdf->mlx.init, WIN_WIDTH, WIN_HEIGHT);
-
+	fdf->image.data = mlx_get_data_addr(fdf->mlx.img, &fdf->image.bpp, \
+			&fdf->image.size, &fdf->image.endian);
 	mlx_destroy_image(fdf->mlx.init, fdf->mlx.image);
 }
 
